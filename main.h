340e769ba26be0db52772f2743c8b89c1884fa88
AymanSdk <ayman.sdk123@outlook.com>
2023-07-24 19:02:05 +0100

    check 5

--- main.h
+++ main.h
@@ -3,18 +3,12 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 
 
 /* main print function */
 int _printf(const char *format, ...);
-
-/* struct to get the correct function to print */
-/*typedef struct print
-{
-    char *type;
-    int (*f)(va_list args);
-} print_data; */
 
 
 /* format handler */
@@ -22,11 +16,9 @@ int format_handler(char specif, va_list args);
 
 
 /* print functions */
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
 
-/* function to get the correct function to print */
+int print_string(va_list args);
+
 
 
 
