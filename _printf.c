ae8e4ce76874c5194f4fe0165bde46741d3ce75a
AymanSdk <ayman.sdk123@outlook.com>
2023-07-24 15:41:15 +0100

    betty Checked

--- _printf.c
+++ _printf.c
@@ -3,47 +3,45 @@
 /**
  * _printf - print anything
  * @format: list of argument types passed to the function
- * Return : number of characters printed
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    int printed_chars = 0;
-    va_list args;
-    va_start(args, format);
+	int printed_chars = 0;
+	va_list args;
 
+	va_start(args, format);
 
-
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-            /*char c = (char)va_arg(args, int);*/
-            printed_chars += print_char(args);
-            }
-            else if (*format == 's')
-            {
-            printed_chars += print_string(args);
-            }
-            else if (*format == '%')
-            {
-            printed_chars += print_percent(args);
-            }
-            else
-            {
-                _putchar('%');
-                printed_chars++;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-            printed_chars++;
-        }
-        format++;
-    }
-    va_end(args);
-    return (printed_chars);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				printed_chars += print_char(args);
+			}
+			else if (*format == 's')
+			{
+				printed_chars += print_string(args);
+			}
+			else if (*format == '%')
+			{
+				printed_chars += print_percent(args);
+			}
+			else
+			{
+				_putchar('%');
+				printed_chars++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (printed_chars);
 }
