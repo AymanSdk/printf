9ba97cc7ce0fb3a6f9237efb295a06d7e2c3c8d4
Ghita Bounouara <ghita.bnr2003@gmail.com>
2023-07-24 07:56:40 -0700

    print string

--- _print_string.c
+++ _print_string.c
@@ -8,13 +8,12 @@
 
 int print_string(va_list args)
 {
-    int i;
-    char *str;
-
-    str = va_arg(args, char *);
-    if (str == NULL)
-        str = "(null)";
-    for (i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
-    return (i);
+int i;
+char *str;
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+return (i);
 }
